f8f42c3c6fe38d6707b1ed0163eda26e1bbe8c45
Serhii Balaba <balabaserhii@gmail.com>
2024-04-13 19:33:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// ===========Typing-annimation================
+
+var typedn = new Typed(".typing", {
+    strings: ["Content Creator", "Video Editor", "Web developer","Content Creator"],
+    typeSpeed: 100,
+    BackSpeed: 50,
+    loop: true
+}
+)
+var typednphone = new Typed(".typing-phone", {
+    strings: ["Content Creator", "Video Editor", "Web developer","Content Creator"],
+    typeSpeed: 100,
+    BackSpeed: 50,
+    loop: true
+}
+)
+var typednew = new Typed(".typing-new", {
+    strings: ["Content Creator", "Video Editor", "Web developer","Content Creator"],
+    typeSpeed: 100,
+    BackSpeed: 40,
+    loop: true
+}
+
+)
+
+
